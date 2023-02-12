2e06e32d0545acfae67035a3c0fd4f2484acefa0
christiaandelrio <60599988+christiaandelrio@users.noreply.github.com>
2023-02-12 22:46:47 +0100

    Add files via upload

---
+++ DELRIO_LEMOS_CHRISTIAN_DWES04_Tarea/mostrar.php
@@ -0,0 +1,56 @@
+<?php session_start();     //Inicio la sesión
+
+
+    if(isset($_POST['borrar'])){    //Borramos las preferencias
+        
+
+        $_SESSION['idioma']="";       
+        $_SESSION['perfil']="";
+        $_SESSION['zona']="";
+        
+        session_destroy();  
+
+        ob_start();  //Recargamos la página
+        echo "<p class='text-danger'>Se han borrado las preferencias de usuario</p>"; //Mostramos mensaje
+    }
+    
+    if(!isset($_SESSION)){
+        echo "<p class='text-danger'>Debes fijar primero las preferencias</p>";
+    }
+
+
+    if(isset($_POST['establecer'])){   //Establecer lleva a preferencias.php
+        header('Location:preferencias.php');
+    }
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <title>Tarea Unidad 4</title>
+  </head>
+
+
+
+  <body>
+  <div class="d-flex flex-column mb-3" >
+        <h1>Preferencias</h1>
+        <form class="form-floating" method="post">
+            <h6>Idioma: <?php if(isset($_SESSION['idioma'])){echo $_SESSION['idioma'];}?></h6>
+            <br>
+            <h6>Perfil Público: <?php if(isset($_SESSION['perfil'])){echo $_SESSION['perfil'];}?></h6>
+            <br>
+            <h6>Zona Horaria: <?php if(isset($_SESSION['zona'])){echo $_SESSION['zona'];}?></h6>
+            <br>
+            <input type="submit" class="btn btn-info" name="establecer" value="Establecer" />
+            <input type="submit" class="btn btn-danger" name="borrar" value="Borrar" />
+        </form>
+    </div>
+  </body>
+</html>
